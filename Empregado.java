2c681386d8c2a0e4f71488af09d731c880134ad8
carlam97 <carmartins04@hotmail.com>
2021-10-28 19:43:33 +0000

    atv 12

---
+++ Empregado.java
@@ -0,0 +1,11 @@
+public class Empregado {
+    public class Empregado extends Pessoa {    // subclasse 
+    
+        private int codigoSetor;
+        private double salarioBase;
+        private double imposto;
+    
+        //fazer os métodos get e set
+    }
+    
+}
